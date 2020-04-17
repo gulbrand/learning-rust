ea0fb4caa905235fc7b02851c39ae8264f9195a2
gulbrand.dev <gulbrand.dev@gmail.com>
2020-04-16 17:17:55 -0700

    Use two counters and one iteration. A lo and hi counter are used to count speculative/required open left parens. If lo is 0 at the end, the string is valid. If hi ever goes below zero during the iteration, the string is invalid.
    https://leetcode.com/problems/valid-parenthesis-string/

--- src/bin/valid_parenthesis_string.rs
+++ src/bin/valid_parenthesis_string.rs
@@ -1,50 +1,32 @@
 
 struct Solution;
 
-struct ParenChecker {
-    paren_stack: Vec<char>,
-    wild_card_stack: Vec<char>
-}
-
-impl ParenChecker {
-    pub fn new() -> ParenChecker {
-        ParenChecker {
-            paren_stack: Vec::new(),
-            wild_card_stack: Vec::new(),
-        }
-    }
-
-    pub fn consume(self: &mut Self, c: char) -> bool {
-        if c == '(' {
-            self.paren_stack.push(c);
-        } else if c == ')' {
-            if let Some(last_c) = self.paren_stack.pop() {
-                if last_c != '(' {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    pub fn is_empty(self: Self) -> bool {
-        self.paren_stack.is_empty()
-    }
-}
 
 #[allow(dead_code)]
 impl Solution {
     pub fn check_valid_string(s: String) -> bool {
-        let mut paren_checker = ParenChecker::new();
+        let mut lo = 0;
+        let mut hi = 0;
         for c in s.chars() {
-            let result = paren_checker.consume(c);
-            if result != true {
+            lo += match c {
+                '(' => 1,
+                _ => -1
+            };
+            hi += match c {
+                ')' => -1,
+                _ => 1
+            };
+            // hi should never go < 0, that indicates under-flow of previous opening left parens.
+            if hi < 0 {
                 return false;
             }
+            // it's ok for lo to go negative, as consuming an '*' as an opening
+            // left paren is speculative and could be a mistake, but ok as long as the rest checks
+            // out.
+            // this covers many cases, such as one or more '*' at the end, in the middle, etc.
+            lo = std::cmp::max(0, lo);
         }
-        return paren_checker.is_empty();
+        lo == 0
     }
 }
 
@@ -80,6 +62,15 @@ pub mod tests {
     }
 
     #[test]
+    pub fn complex_missing_open_left_parens_test_false() {
+        let test_input = "((())))))".to_string();
+        let expected = false;
+        let actual =
+            Solution::check_valid_string(test_input);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
     pub fn simple_test_true() {
         let test_input = "(())".to_string();
         let expected = true;
@@ -88,6 +79,68 @@ pub mod tests {
         assert_eq!(actual, expected);
     }
 
+    #[test]
+    pub fn simple_test_with_wc_true() {
+        let test_input = "((*))".to_string();
+        let expected = true;
+        let actual =
+            Solution::check_valid_string(test_input);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    pub fn complex_test_true() {
+        let test_input = "(*))".to_string();
+        let expected = true;
+        let actual =
+            Solution::check_valid_string(test_input);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    pub fn complex_wc_test_true() {
+        let test_input = "(**))".to_string();
+        let expected = true;
+        let actual =
+            Solution::check_valid_string(test_input);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    pub fn complex_wc_at_end_test_true() {
+        let test_input = "(**))********************".to_string();
+        let expected = true;
+        let actual =
+            Solution::check_valid_string(test_input);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    pub fn complex_wc_in_middle_test_true() {
+        let test_input = "(************************))".to_string();
+        let expected = true;
+        let actual =
+            Solution::check_valid_string(test_input);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    pub fn complex_wc_at_start_test_true() {
+        let test_input = "**************(************************))".to_string();
+        let expected = true;
+        let actual =
+            Solution::check_valid_string(test_input);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    pub fn complex_wc_all_over_test_true() {
+        let test_input = "**(**(*)*))".to_string();
+        let expected = true;
+        let actual =
+            Solution::check_valid_string(test_input);
+        assert_eq!(actual, expected);
+    }
 }
 
 pub fn main() {
