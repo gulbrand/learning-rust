07a87a70eb00d493f017d333e6332cb70bb52741
Mark Gulbrandsen <mark.gulbrandsen@gmail.com>
2020-04-25 11:48:06 -0700

    https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/531/week-4/3310/
    much simpler backtracking solution. the base case is simpler and used to terminate as soon as a valid solution is found, and also does not jump off the end.

--- src/bin/jump_game.rs
+++ src/bin/jump_game.rs
@@ -2,31 +2,22 @@ struct Solution;
 
 impl Solution {
 
-    pub fn bt_helper(nums: &Vec<i32>, index: usize, solution_found: &mut bool) {
-        // is this the end? did we find a solution or not?
-        // println!("{:?}, {}", nums, index);
-        if index >= nums.len()-1 {
-            *solution_found = true;
-        } else {
-            let mut next: Vec<usize> = Vec::new();
-            let jumps = nums[index];
-            for i in (1..jumps+1).rev() {
-                next.push(index+i as usize);
-            }
-            // println!("next = {:?}", next);
-            for n in next {
-                Solution::bt_helper(nums, n, solution_found);
-                if *solution_found {
-                    return;
-                }
+    pub fn can_reach_the_end(nums: &Vec<i32>, index: usize) -> bool {
+        if (index == nums.len() - 1) {
+            return true;
+        }
+        let max_jump_index = std::cmp::min(index + nums[index] as usize, nums.len() - 1);
+        for i in index+1..=max_jump_index {
+            if (Solution::can_reach_the_end(nums, i)) {
+                return true;
             }
         }
+        // println!("{:?}, {}", nums, index);
+        return false;
     }
 
     pub fn can_jump(nums: Vec<i32>) -> bool {
-        let mut solution_found: bool = false;
-        Solution::bt_helper(&nums, 0, &mut solution_found);
-        return solution_found;
+        return Solution::can_reach_the_end(&nums, 0);
     }
 }
 
@@ -46,6 +37,14 @@ pub mod test {
     pub fn simple_test_01() {
         let input = vec![1,0];
         let expected: bool = true;
+        let actual = Solution::can_jump(input);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    pub fn simple_test_02() {
+        let input = vec![2,0,6,9,8,4,5,0,8,9,1,2,9,6,8,8,0,6,3,1,2,2,1,2,6,5,3,1,2,2,6,4,2,4,3,0,0,0,3,8,2,4,0,1,2,0,1,4,6,5,8,0,7,9,3,4,6,6,5,8,9,3,4,3,7,0,4,9,0,9,8,4,3,0,7,7,1,9,1,9,4,9,0,1,9,5,7,7,1,5,8,2,8,2,6,8,2,2,7,5,1,7,9,6];
+        let expected: bool = false;
         let actual = Solution::can_jump(input);
         assert_eq!(actual, expected);
     }
