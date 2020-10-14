e3096f7e1cd0918d8bf78ad201f86d1ae68ccd47
gulbrand.dev <gulbrand.dev@gmail.com>
2020-10-13 18:05:19 -0700

    add max distance

---
+++ src/bin/max_distance_in_arrays.rs
@@ -0,0 +1,69 @@
+struct NaiveSolution {}
+
+impl NaiveSolution {
+
+    pub fn max_distance(arrays: Vec<Vec<i32>>) -> i32 {
+        let mut max_distance = 0;
+
+        for i in 0..arrays.len() {
+            for j in i+1..arrays.len() {
+                max_distance = std::cmp::max(
+                    max_distance,
+                    std::cmp::max(
+                        (arrays[i][0] - arrays[j][arrays[j].len() - 1]).abs(),
+                        (arrays[j][0] - arrays[i][arrays[i].len() - 1]).abs(),
+                    )
+                );
+            }
+        }
+        return max_distance;
+    }
+}
+
+struct FastestSolution {}
+impl FastestSolution {
+
+    pub fn max_distance(arrays: Vec<Vec<i32>>) -> i32 {
+        let mut res = 0;
+        let mut min = arrays[0][0];
+        let mut max = arrays[0][arrays[0].len() - 1];
+        for i in 1..arrays.len() {
+            res = std::cmp::max(
+                res,
+                std::cmp::max(
+                    (min - arrays[i][arrays[i].len()-1]).abs(),
+                    (max - arrays[i][0]).abs()
+                )
+            );
+            min = std::cmp::min(min, arrays[i][0]);
+            max = std::cmp::max(max, arrays[i][arrays[i].len() - 1]);
+        }
+        return res;
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+    #[test]
+    pub fn fastest_easy() {
+        let expected = 4;
+        let input = vec![vec![1,2,3], vec![4,5], vec![1,2,3]];
+
+        let actual = FastestSolution::max_distance(input);
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    pub fn naive() {
+        let expected = 4;
+        let input = vec![vec![1,2,3], vec![4,5], vec![1,2,3]];
+
+        let actual = NaiveSolution::max_distance(input);
+        assert_eq!(actual, expected);
+    }
+}
+
+fn main() {
+    println!("max distance in arrays");
+}
